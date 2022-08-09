488c0dee1eb019d5e1f3a51fe2125ed62fce2e22
Henry Yeary <henmofb@gmail.com>
2022-08-09 18:16:26 -0500

    Updating calculator.js

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,25 +1,54 @@
 let rlSync = require('readline-sync');
 
-console.log('Welcome to Calculator!');
-
-console.log("What's the first number?");
-let num1 = rlSync.question();
-
-console.log("What's the second number?");
-let num2 = rlSync.question();
-
-console.log(`What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide`);
-let operation = rlSync.question();
-
-let output;
-if (operation === '1') {
-  output = Number(num1) + Number(num2);
-} else if (operation === '2') {
-  output = Number(num1) - Number(num2);
-} else if (operation === '3') {
-  output = Number(num1) * Number(num2);
-} else if (operation === '4') {
-  output = Number(num1) / Number(num2);
+function prompt (msg) {
+  console.log(`=> ${msg}`)
 }
 
-console.log(`The result is: ${output}`);
+function invalidNumber(num) {
+  return num.trimStart() === '' || Number.isNaN(Number(num));
+}
+
+prompt('Welcome to the Calculator!');
+
+prompt("What's the first number?");
+let num1 = rlSync.question();
+
+while (invalidNumber(num1)) {
+  prompt("Hmmm... that doesn't look like a valid number.")
+  num1 = rlSync.question();
+}
+
+prompt("What's the second number?");
+let num2 = rlSync.question();
+
+while (invalidNumber(num2)) {
+  prompt("Hmmm... that doesn't look like a valid number.")
+  num2 = rlSync.question();
+}
+
+prompt(`What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide`);
+let operation = rlSync.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt("Must choose 1, 2, 3, or 4.");
+  operation = rlSync.question();
+}
+
+let output;
+switch (operation) {
+  case '1': 
+    output = Number(num1) + Number(num2);
+    break;
+  case '2':
+    output = Number(num1) - Number(num2);
+    break;
+  case '3':
+    output = Number(num1) * Number(num2);
+    break;
+  case '4':
+    output = Number(num1) / Number(num2);
+    break;
+  default:
+}
+
+prompt(`The result is: ${output}`);
