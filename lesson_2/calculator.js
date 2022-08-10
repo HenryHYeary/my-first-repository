4adff439c8085f0523f688467a8314f030543ec5
Henry Yeary <henmofb@gmail.com>
2022-08-09 20:09:37 -0500

    Added JSON file to store messages.

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,3 +1,5 @@
+const MESSAGES = require('./calculator_messages.json');
+
 let rlSync = require('readline-sync');
 
 function prompt (msg) {
@@ -8,31 +10,31 @@ function invalidNumber(num) {
   return num.trimStart() === '' || Number.isNaN(Number(num));
 }
 
-prompt('Welcome to the Calculator!');
+prompt(MESSAGES['welcome']);
 
 while (true) {
 
-prompt("What's the first number?");
+prompt(MESSAGES['firstNumber']);
 let num1 = rlSync.question();
 
 while (invalidNumber(num1)) {
-  prompt("Hmmm... that doesn't look like a valid number.")
+  prompt(MESSAGES['invalidNumber']);
   num1 = rlSync.question();
 }
 
-prompt("What's the second number?");
+prompt(MESSAGES['secondNumber']);
 let num2 = rlSync.question();
 
 while (invalidNumber(num2)) {
-  prompt("Hmmm... that doesn't look like a valid number.")
+  prompt(MESSAGES['invalidNumber']);
   num2 = rlSync.question();
 }
 
-prompt(`What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide`);
+prompt(MESSAGES['operation']);
 let operation = rlSync.question();
 
 while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt("Must choose 1, 2, 3, or 4.");
+  prompt(MESSAGES['invalidOperation']);
   operation = rlSync.question();
 }
 
@@ -56,7 +58,7 @@ switch (operation) {
 
 prompt(`The result is: ${output}`);
 
-prompt("Would you like to perform another calculation? Enter 'y' for yes and 'n' for no.");
+prompt(MESSAGES['continueOrNot']);
 let continueOrDiscontinue = rlSync.question();
 
 if (continueOrDiscontinue[0].toLowerCase() !== 'y') break;
