0d80f00b896cde2f03089d808e43e66389b4b5a6
Henry Yeary <henmofb@gmail.com>
2022-08-12 17:35:00 -0500

    Added grand winner feature

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -18,59 +18,100 @@ function choiceShortcut (string) {
 
 function displayWinner(choice, computerChoice) {
   prompt(`You chose ${choice}, the computer chose ${computerChoice}.`);
-
-if ((choice === 'rock' && computerChoice === 'scissors') ||
-(choice === 'rock' && computerChoice === 'lizard') ||
-(choice === 'scissors' && computerChoice === 'lizard') ||
-(choice === 'lizard' && computerChoice === 'paper') ||
-(choice === 'lizard' && computerChoice === 'spock') ||
-(choice === 'paper' && computerChoice === 'spock') ||
-(choice === 'spock' && computerChoice === 'rock') ||
-(choice === 'spock' && computerChoice === 'scissors') ||
-(choice === 'paper' && computerChoice === 'rock') ||
-(choice === 'scissors' && computerChoice === 'paper')) {
+  if ((choice === 'rock' && computerChoice === 'scissors') ||
+    (choice === 'rock' && computerChoice === 'lizard') ||
+    (choice === 'scissors' && computerChoice === 'lizard') ||
+    (choice === 'lizard' && computerChoice === 'paper') ||
+    (choice === 'lizard' && computerChoice === 'spock') ||
+    (choice === 'paper' && computerChoice === 'spock') ||
+    (choice === 'spock' && computerChoice === 'rock') ||
+    (choice === 'spock' && computerChoice === 'scissors') ||
+    (choice === 'paper' && computerChoice === 'rock') ||
+    (choice === 'scissors' && computerChoice === 'paper')) {
   prompt('You win!');
   } else if ((choice === 'rock' && computerChoice === 'paper') ||
-  (choice === 'lizard' && computerChoice === 'scissors') ||
-  (choice === 'lizard' && computerChoice === 'rock') ||
-  (choice === 'spock' && computerChoice === 'paper') ||
-  (choice === 'spock' && computerChoice === 'lizard') ||
-  (choice === 'paper' && computerChoice === 'lizard') ||
-  (choice === 'scissors' && computerChoice === 'spock') ||
-  (choice === 'rock' && computerChoice === 'spock') ||
-  (choice === 'paper' && computerChoice === 'scissors') ||
-  (choice === 'scissors' && computerChoice === 'rock')) {
+    (choice === 'lizard' && computerChoice === 'scissors') ||
+    (choice === 'lizard' && computerChoice === 'rock') ||
+    (choice === 'spock' && computerChoice === 'paper') ||
+    (choice === 'spock' && computerChoice === 'lizard') ||
+    (choice === 'paper' && computerChoice === 'lizard') ||
+    (choice === 'scissors' && computerChoice === 'spock') ||
+    (choice === 'rock' && computerChoice === 'spock') ||
+    (choice === 'paper' && computerChoice === 'scissors') ||
+    (choice === 'scissors' && computerChoice === 'rock')) {
   prompt('Computer wins!');
     } else {
-    prompt("It's a tie!");
+  prompt("It's a tie!");
+  }
+}
+
+let playerWins = 0;
+let computerWins = 0;
+
+function incrementResult(choice, computerChoice) {
+  if ((choice === 'rock' && computerChoice === 'scissors') ||
+    (choice === 'rock' && computerChoice === 'lizard') ||
+    (choice === 'scissors' && computerChoice === 'lizard') ||
+    (choice === 'lizard' && computerChoice === 'paper') ||
+    (choice === 'lizard' && computerChoice === 'spock') ||
+    (choice === 'paper' && computerChoice === 'spock') ||
+    (choice === 'spock' && computerChoice === 'rock') ||
+    (choice === 'spock' && computerChoice === 'scissors') ||
+    (choice === 'paper' && computerChoice === 'rock') ||
+    (choice === 'scissors' && computerChoice === 'paper')) {
+  return playerWins++;
+  } else if ((choice === 'lizard' && computerChoice === 'scissors') ||
+    (choice === 'lizard' && computerChoice === 'rock') ||
+    (choice === 'spock' && computerChoice === 'paper') ||
+    (choice === 'spock' && computerChoice === 'lizard') ||
+    (choice === 'paper' && computerChoice === 'lizard') ||
+    (choice === 'scissors' && computerChoice === 'spock') ||
+    (choice === 'rock' && computerChoice === 'spock') ||
+    (choice === 'paper' && computerChoice === 'scissors') ||
+    (choice === 'scissors' && computerChoice === 'rock')) {
+  return computerWins++;
   }
 }
 
 while (true) {
 
-prompt(`Choose one: ${VALID_CHOICES.join(', ')} (can enter only first letter for shortcut, only first two letters for spock and scissors)`);
-let choice = readline.question();
-choice = choiceShortcut(choice);
-
-while (!VALID_CHOICES.includes(choice)) {
-  prompt("That's not a valid choice.");
-  choice = readline.question();
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}\n(can enter only first letter for shortcut; only first two letters for spock and scissors)`);
+  let choice = readline.question();
   choice = choiceShortcut(choice);
-}
 
-let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-let computerChoice = VALID_CHOICES[randomIndex];
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice.");
+    choice = readline.question();
+    choice = choiceShortcut(choice);
+  }
 
-displayWinner(choice, computerChoice);
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
 
-prompt('Would you like to play again? "y" for yes, "n" for no.');
-answer = readline.question().toLowerCase();
-while (answer[0] !== 'n' && answer[0] !== 'y') {
-  prompt('Please enter "y" or "n".')
+  displayWinner(choice, computerChoice);
+
+  incrementResult(choice, computerChoice);
+
+  if (playerWins === 3) {
+    prompt('You are the grand winner!');
+    return playerWins = 0;
+    return computerWins = 0;
+  }
+
+  if (computerWins === 3) {
+    prompt('The computer is the grand winner!');
+    return playerWins = 0;
+    return computerWins = 0;
+  }
+
+  prompt('Would you like to play again? "y" for yes, "n" for no.');
   answer = readline.question().toLowerCase();
+  
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "y" or "n".')
+    answer = readline.question().toLowerCase();
+  }
+
+  if (answer[0] !== 'y') break;
+
 }
-
-if (answer[0] !== 'y') break;
-
-}
-
