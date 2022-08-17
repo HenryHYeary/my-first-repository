b572edb50ef91a8bb1b59b48991b213fe582dea2
Henry Yeary <henmofb@gmail.com>
2022-08-16 19:03:15 -0500

    Finished up exercises for practice-problems-easy-2.

--- lesson_3/practice-problems-easy-2.js
+++ lesson_3/practice-problems-easy-2.js
@@ -34,3 +34,37 @@ let fullFamousWords2 = fourScoreAnd.concat(famousWords);
 
 console.log(fullFamousWords1);
 console.log(fullFamousWords2);
+
+let array = [1, 2, 3, 4, 5];
+
+array.splice(2, 1);
+
+console.log(array);
+
+let flintstones = ["Fred", "Wilma"];
+
+let flintstonesFull = flintstones.concat(['Barney', 'Betty', 'Bambam', 'Pebbles']);
+
+console.log(flintstonesFull);
+
+let flintstonesObj = { Fred: 0, Wilma: 1, Barney: 2, Betty: 3, Bambam: 4, Pebbles: 5 };
+
+console.log(Object.entries(flintstonesObj)[2]);
+
+let numbers3 = [1, 2, 3, 4]; // true
+let table = { field1: 1, field2: 2, field3: 3, field4: 4 }; // false
+
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(table));
+
+let title = "Flintstone Family Members";
+
+let padding = Math.floor((40 - title.length) / 2);
+
+console.log(title.padStart(padding + title.length));
+
+let statement1 = "The Flintstones Rock!";
+let statement2 = "Easy come, easy go.";
+
+console.log(statement1.split('').filter(char => char === 't').length);
+console.log(statement2.split('').filter(char => char === 't').length);
