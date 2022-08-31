345063a084d50eb8882860944ef3b07763e6b50c
Henry Yeary <henmofb@gmail.com>
2022-08-31 18:52:44 -0500

    Completed another practice problem.

---
+++ js_101/lesson_5/practice-problem-14.js
@@ -0,0 +1,24 @@
+
+let obj = {
+  grape: { type: 'fruit', colors: ['red', 'green'], size: 'small' },
+  carrot: { type: 'vegetable', colors: ['orange'], size: 'medium' },
+  apple: { type: 'fruit', colors: ['red', 'green'], size: 'medium' },
+  apricot: { type: 'fruit', colors: ['orange'], size: 'medium' },
+  marrow: { type: 'vegetable', colors: ['green'], size: 'large' },
+};
+
+let capitalize = word => word[0].toUpperCase() + word.slice(1);
+
+let objValues = Object.values(obj);
+
+let newArr = objValues.map(attributes => {
+  if (attributes['type'] === 'fruit') {
+    return attributes['colors'].map(char => capitalize(char));
+  } else {
+    return attributes['size'].toUpperCase();
+  }
+});
+
+console.log(newArr);
+
+
