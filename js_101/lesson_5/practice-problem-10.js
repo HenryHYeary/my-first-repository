536d9a10e416b40ca23a7c9411c2e1fb62281713
Henry Yeary <henmofb@gmail.com>
2022-09-01 17:29:28 -0500

    Updating.

--- js_101/lesson_5/practice-problem-10.js
+++ js_101/lesson_5/practice-problem-10.js
@@ -2,20 +2,22 @@
 let arr = [['b', 'c', 'a'], [2, 11, -3], ['blue', 'black', 'green']];
 
 let newArr = arr.map(subArr => {
- return subArr.slice().sort((a, b) => {
   if (typeof subArr[0] === 'number') {
-    return b - a;
+    return subArr.slice().sort((a, b) => b - a);
   }
 
-  if (a < b) {
-    return 1;
-  } else if (a > b) {
-    return -1;
-  } else {
-    return 0;
+  if (typeof subArr[0] === 'string') {
+    return subArr.slice().sort((a, b) => {
+      if (a > b) {
+        return -1;
+      } else if (a < b) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
   }
- });
 });
 
-
 console.log(newArr);
+console.log(arr);
