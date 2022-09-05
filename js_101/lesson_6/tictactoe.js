95ff8a1ae888055b209247194ff606aa921279e1
Henry Yeary <henmofb@gmail.com>
2022-09-05 16:49:11 -0500

    Finished tictactoe.js.

--- js_101/lesson_6/tictactoe.js
+++ js_101/lesson_6/tictactoe.js
@@ -4,19 +4,21 @@ const HUMAN_MARKER = 'X';
 const COMPUTER_MARKER = 'O';
 
 function displayBoard(board) {
+  console.clear();
+
   console.log('');
-console.log('     |     |');
-console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}`);
-console.log('     |     |');
-console.log('-----+-----+-----')
-console.log('     |     |');
-console.log(`  ${board['4']}  |  ${board['5']}  |  ${board['6']}`);
-console.log('     |     |');
-console.log('-----+-----+-----')
-console.log('     |     |');
-console.log(`  ${board['7']}  |  ${board['8']}  |  ${board['9']}`);
-console.log('     |     |');
-console.log('');
+  console.log('     |     |');
+  console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}`);
+  console.log('     |     |');
+  console.log('-----+-----+-----')
+  console.log('     |     |');
+  console.log(`  ${board['4']}  |  ${board['5']}  |  ${board['6']}`);
+  console.log('     |     |');
+  console.log('-----+-----+-----')
+  console.log('     |     |');
+  console.log(`  ${board['7']}  |  ${board['8']}  |  ${board['9']}`);
+  console.log('     |     |');
+  console.log('');
 }
 
 function initializeBoard() {
@@ -35,6 +37,14 @@ function prompt(msg) {
 
 function emptySquares(board) {
   return Object.keys(board).filter(key => board[key] === INITIAL_MARKER);
+}
+
+function boardFull(board) {
+  return emptySquares(board).length === 0;
+}
+
+function someoneWon(board) {
+  return false;
 }
 
 function playerChoosesSquare(board) {
@@ -58,9 +68,52 @@ function computerChoosesSquare(board) {
   board[square] = COMPUTER_MARKER;
 }
 
+function someoneWon(board) {
+  return !!detectWinner(board);
+}
+
+function detectWinner(board) {
+  let winningLines = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9],
+    [1, 4, 7], [2, 5, 8], [3, 6, 9],
+    [1, 5, 7], [3, 5, 7]
+  ];
+
+  for (let i = 0; i < winningLines.length; i++) {
+    let [ sq1, sq2, sq3 ] = winningLines[i];
+
+    if (
+      board[sq1] === HUMAN_MARKER &&
+      board[sq2] === HUMAN_MARKER &&
+      board[sq3] === HUMAN_MARKER
+    ) {
+      return 'Player';
+    } else if (
+      board[sq1] === COMPUTER_MARKER &&
+      board[sq2] === COMPUTER_MARKER &&
+      board[sq3] === COMPUTER_MARKER
+    ) {
+      return 'Computer';
+    }
+  }
+
+  return null;
+}
+
 let board = initializeBoard();
 displayBoard(board);
-playerChoosesSquare(board);
-displayBoard(board);
-computerChoosesSquare(board);
-displayBoard(board);
+
+while (true) {
+
+  playerChoosesSquare(board);
+  computerChoosesSquare(board);
+  displayBoard(board);
+
+  if (someoneWon(board) || boardFull(board)) break;
+}
+
+if (someoneWon(board)) {
+  prompt(`${detectWinner(board)} won!`);
+} else {
+  prompt("It's a tie!");
+}
