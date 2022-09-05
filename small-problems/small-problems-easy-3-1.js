ffc107427e81ba153dfd9265da1d0b9a5f62934c
Henry Yeary <henmofb@gmail.com>
2022-09-04 19:23:44 -0500

    Redid another problem to refresh my memory and liked this solution better than the initial one I came up with.

--- small-problems/small-problems-easy-3-1.js
+++ small-problems/small-problems-easy-3-1.js
@@ -1,23 +1,23 @@
-//input: string
-//ouput: abbreviated string with no repeat letters
-// looking at the example exercises it looks like no repeat characters are allowed (numbers included)
-// if an empty string is passed in we need to return an empty string
+// input: string
+// output: string containing the value of the original string with all consecutive duplicate chars collapsed into a single char
 
-//could split the string into an array and analyze whether the next character is equal to the previous character
-//if the next character is the same then we could tell the program to continue or simply make the condition !=
-//regardless of whether we use an array we need to return a string, either blank string or blank array should suffice as result datatype at the beginning of the program
+// Examples: if a blank string is passed as an argument, the function should return a blank string. Also if a one char argument is passed
+// that one char should be returned as well.
+// Multiple chars also applies to numbers as well.
 
-//Could use a for loop to iterate over every character in string and then add the character to an empty string if it matches the condition
-//could use array methods like forEach if we decide to split the string into an array.
+// Data Structure: should create a blank string within the function to add each character into
+// should also use string.split('') to separate out each character for analysis.
 
+// Algorithm: should use forEach or a for loop to iterate through the split array to verify that the next char ISN'T the same as the current char.
 
 
 function crunch(str) {
   let result = '';
+  let charArr = str.split('');
 
-  for (let i = 0; i <= str.length - 1; i++) {
-    if (str[i] !== str[i + 1]) {
-      result += str[i];
+  for (let i = 0; i < charArr.length; i++) {
+    if (charArr[i] !== charArr[i + 1]) {
+      result += charArr[i];
     }
   }
 
@@ -25,7 +25,7 @@ function crunch(str) {
 }
 
 console.log(crunch('ddaaiillyy ddoouubbllee'));
-console.log(crunch('4444abcabccba'));
-console.log(crunch('ggggggggggggggg'));
-console.log(crunch('a'));
-console.log(crunch(''));
+console.log(crunch('4444abcabccba'));              // "4abcabcba"
+console.log(crunch('ggggggggggggggg'));            // "g"
+console.log(crunch('a'));                          // "a"
+console.log(crunch(''));                           // ""
