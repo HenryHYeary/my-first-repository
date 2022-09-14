07f9218de1eeb8ca4c3c775992a4c362bba57f29
Henry Yeary <henmofb@gmail.com>
2022-09-13 22:49:29 -0500

    Added a few extra notes to my most recent exercise.

--- small-problems/small-problems-list-processing-5.js
+++ small-problems/small-problems-list-processing-5.js
@@ -5,9 +5,11 @@ output: an array of all of the substrings within the string
 Examples: should be ordered by where the substring begins.
 
 Data Structure: should create a new empty variable to add all of the values to.
+create a substring variable to keep track of the current substring you are on (iterate through
+the every substring of each possible length).
 
-Algorithm: use the function from the previous exercise and and apply it to similary created substrings
-for this string/array.
+Algorithm: concatenate the arrays created by the leadingSubstrings function when applied to
+each newly created substring of each possible substring length. 
 */
 
 function substrings(string) {
