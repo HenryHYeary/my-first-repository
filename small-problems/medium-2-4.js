54ff5500e14699f38e7f1426cc2dea6b9a6a4aaa
Henry Yeary <henmo@LAPTOP-A37FGP66.localdomain>
2022-09-21 17:59:37 -0500

    Started PEDAC process for one exercise.

---
+++ small-problems/medium-2-4.js
@@ -0,0 +1,11 @@
+/*
+input: an integer representing a year greater than 1752.
+output: the number of Friday the 13ths that occurred for that given year.
+
+Examples: Can assume that the Gregorian calendar will continue to be in use for
+the foreseeable future.
+
+Data Structure: should declare a variable representing the count of Friday the 13ths for each month.
+
+Algorithm: Need to use Date.prototype methods to iterate through years.
+*/
