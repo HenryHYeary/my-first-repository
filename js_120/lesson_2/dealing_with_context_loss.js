80c3dc2b526d1f18c4208c82ddf4c8c8321dc269
Henry Yeary <henmofb@gmail.com>
2022-11-28 21:35:53 -0600

    Added a file for context loss prac exercises in lesson 2.

---
+++ js_120/lesson_2/dealing_with_context_loss.js
@@ -0,0 +1,79 @@
+let turk = {
+  firstName: 'Christopher',
+  lastName: 'Turk',
+  occupation: 'Surgeon',
+  getDescription() {
+    return this.firstName + ' ' + this.lastName + ' is a ' + this.occupation + '.';
+  }
+};
+
+function logReturnVal(func, context){
+  let returnVal = func.call(context);
+  console.log(returnVal);
+}
+
+// If you want get description to always execute with turk as its context.
+
+let getTurkDescription = turk.getDescription.bind(turk);
+logReturnVal(getTurkDescription);
+
+
+// Fixing list games with an idiomatic variable assignment to this. 
+
+const TESgames = {
+  titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+  seriesTitle: 'The Elder Scrolls',
+  listGames: function() {
+    let self = this;
+    this.titles.forEach(function(title) {
+      console.log(self.seriesTitle + ': ' + title);
+    });
+  }
+};
+
+TESgames.listGames();
+
+// Using forEach extra arg to supply execution context to callback.
+
+const TESgames2 = {
+  titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+  seriesTitle: 'The Elder Scrolls',
+  listGames: function() {
+    this.titles.forEach(function(title) {
+      console.log(this.seriesTitle + ': ' + title);
+    }, this);
+  }
+};
+
+TESgames2.listGames();
+
+// Using arrow function to make sure execution context is supplied to callback.
+
+const TESgames3 = {
+  titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+  seriesTitle: 'The Elder Scrolls',
+  listGames: function() {
+    this.titles.forEach(title => {
+      console.log(this.seriesTitle + ': ' + title);
+    });
+  }
+};
+
+TESgames3.listGames();
+
+let foo = {
+  a: 0,
+  incrementA: function() {
+    function increment() {
+      this.a += 1;
+    }
+
+    increment.call(this);
+  }
+};
+
+foo.incrementA();
+foo.incrementA();
+foo.incrementA();
+
+console.log(foo.a);
