93f7d13b4da7c6835cad045046e43968bbeff7f1
Henry Yeary <henmofb@gmail.com>
2022-11-29 16:25:01 -0600

    Updated code for constructors and protos prac.

--- js_120/lesson_3/constructors_prototypes_prac.js
+++ js_120/lesson_3/constructors_prototypes_prac.js
@@ -1,86 +1,82 @@
 let RECTANGLE = {
-    area: function() {
-      return this.width * this.height;
-    },
-    perimeter: function() {
-      return 2 * (this.width + this.height);
-    },
-  };
-  
-  function Rectangle(width, height) {
-    Object.setPrototypeOf(this, RECTANGLE);
-    this.width = width;
-    this.height = height;
-    this.area = this.area();
-    this.perimeter = this.perimeter();
+  area: function() {
+    return this.width * this.height;
+  },
+  perimeter: function() {
+    return 2 * (this.width + this.height);
   }
-  
-  let rect1 = new Rectangle(2, 3);
-  
-  console.log(rect1.area);
-  console.log(rect1.perimeter);
-  
-  
-  
-  function Circle(radius) {
-    this.radius = radius;
-  }
-  
-  Circle.prototype.area = function() {
-    return Math.PI * Math.pow(this.radius, 2);
-  }
-  
-  let a = new Circle(3);
-  let b = new Circle(4);
-  
-  console.log(a.area().toFixed(2)); // => 28.27
-  console.log(b.area().toFixed(2)); // => 50.27
-  console.log(a.hasOwnProperty('area')); // => false
-  
-  function Ninja() {
+}
+
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.area = RECTANGLE.area.call(this);
+  this.perimeter = RECTANGLE.perimeter.call(this);
+}
+
+let rect1 = new Rectangle(2, 3);
+
+console.log(rect1.area);
+console.log(rect1.perimeter);
+
+function Circle(radius) {
+  this.radius = radius;
+}
+
+Circle.prototype.area = function() {
+  return Math.PI * Math.pow(this.radius, 2);
+}
+
+let a = new Circle(3);
+let b = new Circle(4);
+
+const log = console.log;
+
+log(a.area().toFixed(2));
+log(b.area().toFixed(2));
+log(a.hasOwnProperty('area'));
+
+function Ninja() {
+  this.swung = false;
+}
+
+Ninja.prototype.swing = function() {
+  this.swung = true;
+  return this;
+}
+
+let ninjaA = new Ninja();
+let ninjaB = new Ninja();
+
+log(ninjaA.swing().swung);
+log(ninjaB.swing().swung);
+
+let ninjaA2;
+
+{
+  const Ninja = function() {
     this.swung = false;
+  };
+
+  ninjaA2 = new Ninja();
+}
+
+let ninjaB2 = new ninjaA2.constructor();
+
+log(ninjaA2.constructor === ninjaB2.constructor);
+
+function User(first, last) {
+  if (!(this instanceof User)) {
+    return new User(first, last)
   }
   
-  // Add a swing method to the Ninja prototype which
-  // modifies `swung` and returns the calling object
-  
-  Ninja.prototype.swing = function() {
-    this.swung = true;
-    return this;
-  }
-  
-  let ninjaA = new Ninja();
-  let ninjaB = new Ninja();
-  
-  console.log(ninjaA.swing().swung);      // logs `true`
-  console.log(ninjaB.swing().swung);      // logs `true`
-  
-  let ninjaD;
-  
-  {
-    const Ninja = function() {
-      this.swung = false;
-    };
-  
-    ninjaC = new Ninja();
-  }
-  
-  ninjaD = new ninjaC.constructor();
-  
-  console.log(ninjaC.constructor === ninjaD.constructor); // => true
-  
-  function User(first, last) {
-    if (!(this instanceof User.prototype.constructor)) {
-      return new User(first, last);
-    }
-  
-    this.name = first + ' ' + last;
-  }
-  
-  let name = 'Jane Doe';
-  let user1 = new User('John', 'Doe');
-  let user2 = User('John', 'Doe');
-  
-  console.log(name);         // => Jane Doe
-  console.log(user1.name);   // => John Doe
-  console.log(user2.name);   // => John Doe
+   this.name = first + ' ' + last;
+}
+
+let name = 'Jane Doe';
+let user1 = new User('John', 'Doe');
+let user2 = User('John', 'Doe');
+
+log(name);
+log(user1.name);
+log(user2.name);
