3ff516c455e999e1cfbd7a9f00140d57905fac78
Henry Yeary <henmofb@gmail.com>
2022-12-06 17:17:14 -0600

    Added alternate commented out solution to first problem in object_creation_patterns.

--- js_120_exercises/object_creation_patterns/exercise_1.js
+++ js_120_exercises/object_creation_patterns/exercise_1.js
@@ -33,8 +33,37 @@ qux.name = 'qux';
 
 const log = console.log;
 
+log(qux.ancestors());  // returns ['baz', 'bar', 'foo', 'Object.prototype']
+log(baz.ancestors());  // returns ['bar', 'foo', 'Object.prototype']
+log(bar.ancestors());  // returns ['foo', 'Object.prototype']
+log(foo.ancestors());  // returns ['Object.prototype']
+
+
+// Alternative recursive solution provided by course
+
+/*
+Object.prototype.ancestors = function ancestors() {
+  let ancestor = Object.getPrototypeOf(this);
+
+  if (Object.prototype.hasOwnProperty.call(this, 'name')) {
+    return [ancestor.name].concat(ancestor.ancestors());
+  }
+
+  return ['Object.prototype'];
+}
+
+let foo = {name: 'foo'};
+let bar = Object.create(foo);
+bar.name = 'bar';
+let baz = Object.create(bar);
+baz.name = 'baz';
+let qux = Object.create(baz);
+qux.name = 'qux';
+
+const log = console.log;
 
 log(qux.ancestors());  // returns ['baz', 'bar', 'foo', 'Object.prototype']
 log(baz.ancestors());  // returns ['bar', 'foo', 'Object.prototype']
 log(bar.ancestors());  // returns ['foo', 'Object.prototype']
 log(foo.ancestors());  // returns ['Object.prototype']
+*/
