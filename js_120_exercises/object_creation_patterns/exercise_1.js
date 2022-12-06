c7a043cd566b049b494d4197839dfbf0cc76d5bf
Henry Yeary <henmofb@gmail.com>
2022-12-06 14:48:19 -0600

    Added solution for first problem on object creation patterns set.

--- js_120_exercises/object_creation_patterns/exercise_1.js
+++ js_120_exercises/object_creation_patterns/exercise_1.js
@@ -2,24 +2,28 @@
 String representation: must change toString()? Or assign Object.prototype
 a name property
 
-while obj isn't null
-  obj is reassigned to it's prototype
-  obj is added to an array
+while obj has a name property
+  obj's name is added to the array
+  obj is reset to it's prototype
+
+array of all objs is returned without the method invoked obj
+is combined with Object.prototype
 */
 
-// name property added to make objects easier to identify
-let foo = {name: 'foo'};
-foo.ancestors = function() {
-  let resultArr = [];
 
-  let object = foo;
-  while (object !== Object.prototype) {
-    object = Object.getPrototypeOf(object);
-    resultArr.push(object.name);
+Object.prototype.ancestors = function() {
+  let ancestorsArr = [];
+  let ancestor = this;
+
+  while (ancestor.hasOwnProperty('name')) {
+    ancestorsArr.push(ancestor.name);
+    ancestor = Object.getPrototypeOf(ancestor);
   }
 
-  return resultArr;
+  return ancestorsArr.filter(name => name !== this.name).concat(['Object.prototype']);
 }
+// name property added to make objects easier to identify
+let foo = {name: 'foo'};
 let bar = Object.create(foo);
 bar.name = 'bar';
 let baz = Object.create(bar);
@@ -29,7 +33,8 @@ qux.name = 'qux';
 
 const log = console.log;
 
+
 log(qux.ancestors());  // returns ['baz', 'bar', 'foo', 'Object.prototype']
-baz.ancestors();  // returns ['bar', 'foo', 'Object.prototype']
-bar.ancestors();  // returns ['foo', 'Object.prototype']
-foo.ancestors();  // returns ['Object.prototype']
+log(baz.ancestors());  // returns ['bar', 'foo', 'Object.prototype']
+log(bar.ancestors());  // returns ['foo', 'Object.prototype']
+log(foo.ancestors());  // returns ['Object.prototype']
