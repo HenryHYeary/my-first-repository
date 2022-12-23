8edae1c6755e729585632ded3444953d9559a738
Henry Yeary <henmofb@gmail.com>
2022-12-22 20:44:13 -0600

    Finished up closure prac problems.

--- js_130/lesson_2/closure-prac-problems.js
+++ js_130/lesson_2/closure-prac-problems.js
@@ -33,7 +33,9 @@ add(6);
 // Exercise 8
 
 function later(func, arg) {
-  return () => func(arg);
+  return function() {
+    return func(arg);
+  }
 }
 
 const logger = message => console.log(message);
@@ -43,5 +45,32 @@ logWarning();
 // Exercise 9
 
 function later2(func, arg) {
-
+  return function(nextArg) {
+    return func(arg, nextArg);
+  }
 }
+
+const notify = function(message, when) {
+  console.log(`${message} in ${when} minutes!`);
+};
+
+let shutdownWarning = later2(notify, "The system is shutting down");
+shutdownWarning(30);
+
+// Exercise 10
+
+"use strict";
+
+function bind(object, func) {
+  return function() {
+    return func.call(object);
+  } 
+}
+
+let obj = {}
+let boundFunc = bind(obj, function() {
+  this.foo = 'bar';
+});
+
+boundFunc();
+console.log(obj);
