91a6e17a6fc10e86f2ac1eeb08684ba51f66edcc
Henry Yeary <henmo@LAPTOP-A37FGP66.localdomain>
2022-12-26 15:49:09 -0600

    Added two more exercise files for 130.

---
+++ js_130_exercises/closures-private-data-iifes.js/exercise-5.js
@@ -0,0 +1,81 @@
+const LETTERS_AND_NUMS = 'abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+
+function setDisplayName() {
+  return function() {
+    let displayName = '';
+    for (let count = 1; count <= 16; count++) {
+      let randomIndex = Math.floor(Math.random() * LETTERS_AND_NUMS.length);
+      displayName += LETTERS_AND_NUMS[randomIndex];
+    }
+
+    return displayName;
+  }
+}
+
+const Account = {
+  init(email, password, firstName, lastName) {
+    let setter = setDisplayName();
+    this.myEmail = email;
+    this.password = password;
+    this.myFirstName = firstName;
+    this.myLastName = lastName;
+    this.displayName = setter();
+    return this;
+  },
+
+  reanonymize(password) {
+    let setter = setDisplayName();
+    if (this.password === password) {
+      this.displayName = setter();
+      return true;
+    } else {
+      return 'Invalid Password';
+    }
+  },
+
+  resetPassword(password, newPassword) {
+    if (this.password === password) {
+      this.password = newPassword;
+      return true;
+    } else {
+      return 'Invalid Password';
+    }
+  },
+
+  firstName(password) {
+    if (this.password === password) {
+      return this.myFirstName;
+    } else {
+      return 'Invalid Password';
+    }
+  },
+
+  lastName(password) {
+    if (this.password === password) {
+      return this.myLastName;
+    } else {
+      return 'Invalid Password';
+    }
+  },
+
+  email(password) {
+    if (this.password === password) {
+      return this.myEmail;
+    } else {
+      return 'Invalid Password';
+    }
+  },
+};
+
+let fooBar = Object.create(Account).init('foo@bar.com', '123456', 'foo', 'bar');
+console.log(fooBar.firstName);                     // returns the firstName function
+console.log(fooBar.email);                         // returns the email function
+console.log(fooBar.firstName('123456'));           // logs 'foo'
+console.log(fooBar.firstName('abc'));              // logs 'Invalid Password'
+console.log(fooBar.displayName);                   // logs 16 character sequence
+console.log(fooBar.resetPassword('123', 'abc'))    // logs 'Invalid Password'
+console.log(fooBar.resetPassword('123456', 'abc')) // logs true
+
+let displayName = fooBar.displayName;
+fooBar.reanonymize('abc');                         // returns true
+console.log(displayName === fooBar.displayName);   // logs false
