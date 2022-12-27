971f97e6f6e30043a482576de3134429079b6a24
Henry Yeary <henmo@LAPTOP-A37FGP66.localdomain>
2022-12-26 21:02:22 -0600

    Updated exercise 6 file.

--- js_130_exercises/closures-private-data-iifes/exercise_6.js
+++ js_130_exercises/closures-private-data-iifes/exercise_6.js
@@ -1,21 +1,18 @@
-/*
-
-*/
-
 const ItemManager = {
   items: [],
-
+  
+  // Need to add verification for item properties
   create(name, category, quantity) {
     this.name = name;
     this.category = category;
     this.quantity = quantity;
     
     this.SKU = (this.name.replace(' ', '').slice(0, 3) + this.category.slice(0, 2)).toUpperCase();
-    items.push(this);
+    this.items.push(this);
   },
 
   _findBySKU(SKU) {
-    return items.find(item => item.SKU === SKU);
+    return this.items.find(item => item.SKU === SKU);
   },
 
   update(SKU, obj) {
@@ -25,11 +22,11 @@ const ItemManager = {
 
   delete(SKU) {
     let correctItem = this._findBySKU(SKU);
-    items.splice(items.indexOf(correctItem), 1);
+    this.items.splice(this.items.indexOf(correctItem), 1);
   },
 
   inStock() {
-    items.forEach(item => {
+    this.items.forEach(item => {
       if (item.quantity > 0) {
         console.log(item.name);
       }
@@ -37,10 +34,31 @@ const ItemManager = {
   },
 
   itemsInCategory(category) {
-    let correctItems = items.filter(item => item.category === category);
+    let correctItems = this.items.filter(item => item.category === category);
     correctItems.forEach(item => console.log(item.name));
   },
 }
 
-ItemManager.create('soccer ball', 'sports', 5);
+const ReportManager = {
+  init(itemManager) {
+    this.items = itemManager;
+  },
+
+  createReporter(SKU) {
+    return {
+      itemInfo() {
+        let item = this.items._findBySKU(SKU)
+      }
+    }
+  }
+}
+
+ItemManager.create('basket ball', 'sports', 0);           // valid item
+ItemManager.create('asd', 'sports', 0);
+ItemManager.create('soccer ball', 'sports', 5);           // valid item
+ItemManager.create('football', 'sports');
+ItemManager.create('football', 'sports', 3);              // valid item
+ItemManager.create('kitchen pot', 'cooking items', 0);
+ItemManager.create('kitchen pot', 'cooking', 3);          // valid item
+// returns list with the 4 valid items
 console.log(ItemManager.items);
