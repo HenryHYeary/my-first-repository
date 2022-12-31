b60e383a8f0e17c0121832d74c1434504d4f54b0
Henry Yeary <henmo@LAPTOP-A37FGP66.localdomain>
2022-12-30 18:43:59 -0600

    Updated exercise files and practice problem files.

---
+++ js_130/lesson_5/setInterval-prac-problems.js
@@ -0,0 +1,21 @@
+// Problem 1
+function startCounting() {
+  let count = 0;
+
+  let counterId = setInterval(function () {
+    count += 1;
+    console.log(count);
+  }, 1000);
+
+  return counterId;
+}
+
+function stopCounting(counterId) {
+  clearInterval(counterId);
+}
+
+let counterId = startCounting();
+
+stopCounting(counterId);
+
+
--- js_130_exercises/closures-private-data-iifes/exercise_6.js
+++ js_130_exercises/closures-private-data-iifes/exercise_6.js
@@ -1,64 +1,158 @@
-const ItemManager = {
-  items: [],
-  
-  // Need to add verification for item properties
-  create(name, category, quantity) {
-    this.name = name;
-    this.category = category;
-    this.quantity = quantity;
-    
-    this.SKU = (this.name.replace(' ', '').slice(0, 3) + this.category.slice(0, 2)).toUpperCase();
-    this.items.push(this);
-  },
+const ItemManager = (function() {
+  let items = [];
 
-  _findBySKU(SKU) {
-    return this.items.find(item => item.SKU === SKU);
-  },
-
-  update(SKU, obj) {
-    let correctItem = this._findBySKU(SKU);
-    Object.assign(correctItem, obj);
-  },
-
-  delete(SKU) {
-    let correctItem = this._findBySKU(SKU);
-    this.items.splice(this.items.indexOf(correctItem), 1);
-  },
-
-  inStock() {
-    this.items.forEach(item => {
-      if (item.quantity > 0) {
-        console.log(item.name);
-      }
-    });
-  },
-
-  itemsInCategory(category) {
-    let correctItems = this.items.filter(item => item.category === category);
-    correctItems.forEach(item => console.log(item.name));
-  },
-}
-
-const ReportManager = {
-  init(itemManager) {
-    this.items = itemManager;
-  },
-
-  createReporter(SKU) {
-    return {
-      itemInfo() {
-        let item = this.items._findBySKU(SKU)
-      }
-    }
+  function validateItemName(name) {
+    let onlyChars = name.replace(' ', '');
+    return onlyChars.length >= 5;
   }
-}
 
-ItemManager.create('basket ball', 'sports', 0);           // valid item
-ItemManager.create('asd', 'sports', 0);
-ItemManager.create('soccer ball', 'sports', 5);           // valid item
-ItemManager.create('football', 'sports');
-ItemManager.create('football', 'sports', 3);              // valid item
-ItemManager.create('kitchen pot', 'cooking items', 0);
-ItemManager.create('kitchen pot', 'cooking', 3);          // valid item
-// returns list with the 4 valid items
-console.log(ItemManager.items);
+  function validateCategory(category) {
+    if (category.includes(' ')) return false;
+    return category.length >= 5;
+  }
+
+  function validateQuantity(quantity) {
+    return quantity !== undefined;
+  }
+
+  function validateItem(name, category, quantity) {
+    return (validateItemName(name) && validateCategory(category)) &&
+    validateQuantity(quantity);
+  }
+
+  function findBySKU(SKU) {
+    return items.find(item => item.SKU === SKU);
+  }
+
+  return {
+    create(SKU, itemName, category, quantity) {
+      if (validateItem(SKU, itemName, category, quantity)) {
+        this.SKU = SKU;
+        this.itemName = itemName;
+        this.category = category;
+        this.quantity = quantity;
+        return this;
+      } else {
+        return false;
+      }
+    },
+
+    update(SKU) {
+      if (SKU === this.SKU) {
+        
+      }
+    },
+  }
+})();
+
+  // function update(SKU, obj) {
+  //   let correctItem = findBySKU(SKU);
+  //   Object.assign(correctItem, obj);
+  // }
+
+  // function delete(SKU) {
+  //   let correctItem = findBySKU(SKU);
+  //   this.items.splice(this.items.indexOf(correctItem), 1);
+  // },
+
+  // inStock() {
+  //   this.items.forEach(item => {
+  //     if (item.quantity > 0) {
+  //       console.log(item.name);
+  //     }
+  //   });
+  // },
+
+  // itemsInCategory(category) {
+  //   let correctItems = this.items.filter(item => item.category === category);
+  //   correctItems.forEach(item => console.log(item.name));
+  // }
+  
+//   create(name, category, quantity) {
+//     if (this._validateItem(name, category, quantity)) {
+//     let item = {
+//       name,
+//       category,
+//       quantity,
+
+//       SKU: (name.replace(' ', '').slice(0, 3) + category.slice(0, 2)).toUpperCase(),
+//     };
+
+//     this.items.push(item);
+//     } else {
+//       return false;
+//     }
+//   },
+
+//   _validateItemName(name) {
+//     let onlyChars = name.replace(' ', '');
+//     return onlyChars.length >= 5;
+//   },
+
+//   _validateCategory(category) {
+//     if (category.includes(' ')) return false;
+//     return category.length >= 5;
+//   },
+
+//   _validateQuantity(quantity) {
+//     return quantity !== undefined;
+//   },
+
+//   _validateItem(name, category, quantity) {
+//     return (this._validateItemName(name) && this._validateCategory(category)) &&
+//     this._validateQuantity(quantity);
+//   },
+
+//   _findBySKU(SKU) {
+//     return this.items.find(item => item.SKU === SKU);
+//   },
+
+//   update(SKU, obj) {
+//     let correctItem = this._findBySKU(SKU);
+//     Object.assign(correctItem, obj);
+//   },
+
+//   delete(SKU) {
+//     let correctItem = this._findBySKU(SKU);
+//     this.items.splice(this.items.indexOf(correctItem), 1);
+//   },
+
+//   inStock() {
+//     this.items.forEach(item => {
+//       if (item.quantity > 0) {
+//         console.log(item.name);
+//       }
+//     });
+//   },
+
+//   itemsInCategory(category) {
+//     let correctItems = this.items.filter(item => item.category === category);
+//     correctItems.forEach(item => console.log(item.name));
+//   },
+// }
+
+// const ReportManager = {
+//   init(itemManager) {
+//     this.items = itemManager.items;
+//   },
+
+//   createReporter(SKU) {
+//     return {
+//       itemInfo() {
+//         let item = this.items._findBySKU(SKU)
+//       }
+//     }
+//   }
+// }
+
+// const log = console.log;
+
+// ItemManager.create('basket ball', 'sports', 0);           // valid item
+// log(ItemManager.create('asd', 'sports', 0));
+// ItemManager.create('soccer ball', 'sports', 5);           // valid item
+// log(ItemManager.create('football', 'sports'));
+// ItemManager.create('football', 'sports', 3);              // valid item
+// log(ItemManager.create('kitchen pot', 'cooking items', 0));
+// ItemManager.create('kitchen pot', 'cooking', 3);          // valid item
+// // returns list with the 4 valid items
+// console.log(ItemManager.items);
