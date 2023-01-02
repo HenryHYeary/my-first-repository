b25344e6a165bda3ed72314895c406528555b3b9
Henry Yeary <henmofb@gmail.com>
2023-01-02 14:58:06 -0600

    Finished up last exercise for private data.

--- js_130_exercises/closures-private-data-iifes/exercise-5.js
+++ js_130_exercises/closures-private-data-iifes/exercise-5.js
@@ -79,3 +79,94 @@ console.log(fooBar.resetPassword('123456', 'abc')) // logs true
 let displayName = fooBar.displayName;
 fooBar.reanonymize('abc');                         // returns true
 console.log(displayName === fooBar.displayName);   // logs false
+
+// Using an IIFE
+
+// const Account = (function() {
+//   function randomLetterNumber() {
+//     let lettersAndNums = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+//     let randomIndex = Math.floor(Math.random() * lettersAndNums.length);
+//     return lettersAndNums[randomIndex];
+//   }
+
+//   function anonymize() {
+//     let result = '';
+
+//     for (let count = 1; count <= 16; count++) {
+//       result += randomLetterNumber();
+//     }
+
+//     return result;
+//   }
+
+//   return {
+//     init(email, password, firstName, lastName) {
+//       this.userEmail = email;
+//       this.userPassword = password;
+//       this.userFirstName = firstName;
+//       this.userLastName = lastName;
+//       this.displayName = anonymize();
+//       return this;
+//     },
+
+//     reanonymize(password) {
+//       if (this._validatePassword(password)) {
+//         this.displayName = anonymize();
+//         return true;
+//       } else {
+//         return 'Invalid Password';
+//       }
+//     },
+
+//     resetPassword(currentPassword, newPassword) {
+//       if (this._validatePassword(currentPassword)) {
+//         this.userPassword = newPassword;
+//         return true;
+//       } else {
+//         return 'Invalid Password';
+//       }
+//     },
+
+//     firstName(password) {
+//       if (this._validatePassword(password)) {
+//         return this.userFirstName;
+//       } else {
+//         return 'Invalid Password';
+//       }
+//     },
+
+//     lastName(password) {
+//       if (this._validatePassword(password)) {
+//         return this.userLastName;
+//       } else {
+//         return 'Invalid Password';
+//       }
+//     },
+
+//     email(password) {
+//       if (this._validatePasswordvalidatePassword(password)) {
+//         return this.userEmail;
+//       } else {
+//         return 'Invalid Password';
+//       }
+//     },
+
+//     _validatePassword(password) {
+//       return this.userPassword === password;
+//     }
+//   }
+// })();
+
+// let fooBar = Object.create(Account).init('foo@bar.com', '123456', 'foo', 'bar');
+// const log = console.log;
+// log(fooBar.firstName);
+// log(fooBar.email);
+// log(fooBar.firstName('123456'));
+// log(fooBar.firstName('abc'));
+// log(fooBar.displayName);
+// log(fooBar.resetPassword('123', 'abc'));
+// log(fooBar.resetPassword('123456', 'abc'));
+
+// let displayName = fooBar.displayName;
+// fooBar.reanonymize('abc');
+// log(displayName === fooBar.displayName);
--- js_130_exercises/closures-private-data-iifes/exercise_6.js
+++ js_130_exercises/closures-private-data-iifes/exercise_6.js
@@ -1,9 +1,11 @@
-const ItemManager = (function() {
-  let items = [];
-
+const ItemCreator = (function() {
   function validateItemName(name) {
     let onlyChars = name.replace(' ', '');
     return onlyChars.length >= 5;
+  }
+
+  function validateQuantity(quantity) {
+    return quantity !== undefined;
   }
 
   function validateCategory(category) {
@@ -11,148 +13,115 @@ const ItemManager = (function() {
     return category.length >= 5;
   }
 
-  function validateQuantity(quantity) {
-    return quantity !== undefined;
+  function generateSKUCode(name, category) {
+    let nameSlice = name.slice(0, 3);
+    let categorySlice = category.slice(0, 2);
+
+    return (nameSlice + categorySlice).toUpperCase();
   }
 
-  function validateItem(name, category, quantity) {
-    return (validateItemName(name) && validateCategory(category)) &&
-    validateQuantity(quantity);
+  return function(itemName, category, quantity) {
+    if (validateItemName(itemName) && validateCategory(category) && validateQuantity(quantity)) {
+      this.SKUCode = generateSKUCode(itemName, category);
+      this.itemName = itemName;
+      this.category = category;
+      this.quantity = quantity;
+    } else {
+      return { notValid: true };
+    }
   }
 
-  function findBySKU(SKU) {
-    return items.find(item => item.SKU === SKU);
-  }
-
-  return {
-    create(SKU, itemName, category, quantity) {
-      if (validateItem(SKU, itemName, category, quantity)) {
-        this.SKU = SKU;
-        this.itemName = itemName;
-        this.category = category;
-        this.quantity = quantity;
-        return this;
-      } else {
-        return false;
-      }
-    },
-
-    update(SKU) {
-      if (SKU === this.SKU) {
-        
-      }
-    },
-  }
 })();
 
-  // function update(SKU, obj) {
-  //   let correctItem = findBySKU(SKU);
-  //   Object.assign(correctItem, obj);
-  // }
+const ItemManager = {
+  items: [],
 
-  // function delete(SKU) {
-  //   let correctItem = findBySKU(SKU);
-  //   this.items.splice(this.items.indexOf(correctItem), 1);
-  // },
+  getItem(SKU) {
+    return this.items.find(item => item.SKUCode === SKU);
+  },
 
-  // inStock() {
-  //   this.items.forEach(item => {
-  //     if (item.quantity > 0) {
-  //       console.log(item.name);
-  //     }
-  //   });
-  // },
-
-  // itemsInCategory(category) {
-  //   let correctItems = this.items.filter(item => item.category === category);
-  //   correctItems.forEach(item => console.log(item.name));
-  // }
+  create(itemName, category, quantity) {
+    let item = new ItemCreator(itemName, category, quantity)
   
-//   create(name, category, quantity) {
-//     if (this._validateItem(name, category, quantity)) {
-//     let item = {
-//       name,
-//       category,
-//       quantity,
+    if (!item.hasOwnProperty('notValid')) {
+      this.items.push(item);
+    } else {
+      return false;
+    }
+  },
 
-//       SKU: (name.replace(' ', '').slice(0, 3) + category.slice(0, 2)).toUpperCase(),
-//     };
+  update(SKU, obj) {
+    let targetItem = this.getItem(SKU);
+    Object.assign(targetItem, obj);
+  },
 
-//     this.items.push(item);
-//     } else {
-//       return false;
-//     }
-//   },
+  delete(SKU) {
+    let targetItem = this.getItem(SKU);
+    this.items.splice(this.items.indexOf(targetItem), 1);
+  },
 
-//   _validateItemName(name) {
-//     let onlyChars = name.replace(' ', '');
-//     return onlyChars.length >= 5;
-//   },
+  inStock() {
+    let stockedItems = this.items.filter(item => item.quantity > 0);
+    stockedItems.forEach(item => console.log(item.itemName));
+  },
 
-//   _validateCategory(category) {
-//     if (category.includes(' ')) return false;
-//     return category.length >= 5;
-//   },
+  itemsInCategory(category) {
+    let sameCatItems = this.items.filter(item => item.category === category);
+    sameCatItems.forEach(item => console.log(item.itemName));
+  },
+};
 
-//   _validateQuantity(quantity) {
-//     return quantity !== undefined;
-//   },
+const ReportManager = {
+  init(itemManager) {
+    this.items = itemManager;
+  },
 
-//   _validateItem(name, category, quantity) {
-//     return (this._validateItemName(name) && this._validateCategory(category)) &&
-//     this._validateQuantity(quantity);
-//   },
+  createReporter(SKU) {
+    let targetItem = this.items.getItem(SKU);
 
-//   _findBySKU(SKU) {
-//     return this.items.find(item => item.SKU === SKU);
-//   },
+    return {
+      itemInfo() {
+        let itemEntries = Object.entries(targetItem);
+        itemEntries.forEach(entry => {
+          console.log(`${entry[0]}: ${entry[1]}`);
+        });
+      }
+    }
+  },
 
-//   update(SKU, obj) {
-//     let correctItem = this._findBySKU(SKU);
-//     Object.assign(correctItem, obj);
-//   },
+  reportInStock() {
+    this.items.inStock();
+  },
+};
 
-//   delete(SKU) {
-//     let correctItem = this._findBySKU(SKU);
-//     this.items.splice(this.items.indexOf(correctItem), 1);
-//   },
+const log = console.log;
 
-//   inStock() {
-//     this.items.forEach(item => {
-//       if (item.quantity > 0) {
-//         console.log(item.name);
-//       }
-//     });
-//   },
+ItemManager.create('basket ball', 'sports', 0);           // valid item
+log(ItemManager.create('asd', 'sports', 0));
+ItemManager.create('soccer ball', 'sports', 5);           // valid item
+log(ItemManager.create('football', 'sports'));
+ItemManager.create('football', 'sports', 3);              // valid item
+log(ItemManager.create('kitchen pot', 'cooking items', 0));
+ItemManager.create('kitchen pot', 'cooking', 3);          // valid item
+// returns list with the 4 valid items
+console.log(ItemManager.items);
 
-//   itemsInCategory(category) {
-//     let correctItems = this.items.filter(item => item.category === category);
-//     correctItems.forEach(item => console.log(item.name));
-//   },
-// }
+ReportManager.init(ItemManager);
+ReportManager.reportInStock();
 
-// const ReportManager = {
-//   init(itemManager) {
-//     this.items = itemManager.items;
-//   },
+ItemManager.update('SOCSP', { quantity: 0 });
+ItemManager.inStock();
 
-//   createReporter(SKU) {
-//     return {
-//       itemInfo() {
-//         let item = this.items._findBySKU(SKU)
-//       }
-//     }
-//   }
-// }
+ReportManager.reportInStock();
 
-// const log = console.log;
+ItemManager.itemsInCategory('sports');
 
-// ItemManager.create('basket ball', 'sports', 0);           // valid item
-// log(ItemManager.create('asd', 'sports', 0));
-// ItemManager.create('soccer ball', 'sports', 5);           // valid item
-// log(ItemManager.create('football', 'sports'));
-// ItemManager.create('football', 'sports', 3);              // valid item
-// log(ItemManager.create('kitchen pot', 'cooking items', 0));
-// ItemManager.create('kitchen pot', 'cooking', 3);          // valid item
-// // returns list with the 4 valid items
-// console.log(ItemManager.items);
+ItemManager.delete('SOCSP');
+
+console.log(ItemManager.items);
+
+let kitchenPotReporter = ReportManager.createReporter('KITCO');
+kitchenPotReporter.itemInfo();
+
+ItemManager.update('KITCO', { quantity: 10 });
+kitchenPotReporter.itemInfo();
