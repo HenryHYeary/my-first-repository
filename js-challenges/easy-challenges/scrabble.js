42f2b88a7841ae5ec02de72af311490332dc5e0b
Henry Yeary <henmofb@gmail.com>
2023-01-02 21:35:01 -0600

    Added solution and test files for scrabble challenge.

---
+++ js-challenges/easy-challenges/scrabble.js
@@ -0,0 +1,57 @@
+class Scrabble {
+  static LETTER_VALUES = {
+    'aeioulnrst': 1,
+    'dg': 2,
+    'bcmp': 3,
+    'fhvwy': 4,
+    'k': 5,
+    'jx': 8,
+    'qz': 10,
+  };
+
+  static score(word) {
+    word = word.replace(' ', '');
+
+    if (word === null) {
+      return 0;
+    }
+
+    let totalScore = 0;
+
+    for (let index = 0; index < word.length; index++) {
+      totalScore += Scrabble.findLetterScore(word[index]);
+    }
+
+    return totalScore;
+  }
+
+  static findLetterScore(letter) {
+    for (let key in Scrabble.LETTER_VALUES) {
+      if (key.includes(letter.toLowerCase())) {
+        return Scrabble.LETTER_VALUES[key];
+      }
+    }
+  }
+
+  constructor(word) {
+    this.word = word;
+  }
+
+  score() {
+    if (this.word === null) {
+      return 0;
+    }
+
+    this.word = this.word.trim();
+
+    let totalScore = 0;
+
+    for (let index = 0; index < this.word.length; index++) {
+      totalScore += Scrabble.findLetterScore(this.word[index]);
+    }
+
+    return totalScore;
+  }
+}
+
+module.exports = Scrabble;
