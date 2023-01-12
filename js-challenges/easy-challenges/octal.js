ce4d5fd0f5e5a939cd2b739e87425dbb4652fd6b
Henry Yeary <henmofb@gmail.com>
2023-01-12 11:01:06 -0600

    Improved solution to octal challenge.

--- js-challenges/easy-challenges/octal.js
+++ js-challenges/easy-challenges/octal.js
@@ -1,44 +1,46 @@
 /*
-input: an integer in string format
-output: a number
+input: a string representing an octal number
+output: the decimal form of that octal string
 
-Examples: string should be the original octal form of the number passed in
-while the output number should be the decimal conversion of the octal.
-Any non number, mixed digit and letter string, or number that includes a digit above
-7 should be considered an invalid octal and return a value of 0.
+Examples: Invalid octals should be converted to a zero value
 
-Data Structure: should verify that the string only contains digits first.
-Use a guard clause to enforce this. After this has been verified, split the string
-into an array and convert every digit to its number form.
+Data Structure: should create a class named octal that takes a string
+to represent its octal value
+Constructor should have only one method named toDecimal and it should
+recognize if the string contains any digits above 7, if it does then the
+value is zero
 
-Algorithm: verify that string only contains digits
-iterate through each digit and multiply it by 8 to the power of the length of the
-original string - 1 decrementing downward for each position.
+Algorithm: split string into digits
+check to see if any digits are above 7
+  if they are then return 0
+end
 
-add the result of this multiplication to a total sum.
+declare varialbe representing sum
 
-Return the total sum
+reverse array and multiply it to the power of 8 based
+on index position adding to sum variable at each step
+
+return sum variable
 */
 
 class Octal {
-  static DIGITS = '1234567890';
-
-  constructor(octalString) {
-    this.octalString = octalString;
+  constructor(octString) {
+    this.octString = octString;
   }
 
   toDecimal() {
-    let octalArr = this.octalString.split('');
-    let decimalSum = 0;
+    let maybeInvalid = this.octString.match(/[^0-7]/g) || [];
 
-    if (!octalArr.every(char => Octal.DIGITS.includes(char)) ||
-    octalArr.some(char => Number(char) > 7)) return 0;
+    if (maybeInvalid.length > 0) return 0;
 
-    octalArr.forEach((digit, index) => {
-      decimalSum += digit * Math.pow(8, (octalArr.length - 1) - index);
-    });
+    let digitsArr = this.octString.split('').reverse();
+    let totalSum = 0;
 
-    return decimalSum;
+    for (let index = 0; index < digitsArr.length; index++) {
+      totalSum += digitsArr[index] * Math.pow(8, index);
+    }
+
+    return totalSum;
   }
 }
 
