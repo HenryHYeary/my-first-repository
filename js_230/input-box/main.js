e11660811cb28b34452af6b5a18f19d7729bb0e1
Henry Yeary <henmofb@gmail.com>
2023-05-11 09:06:54 -0500

    Added input-box exercise.

--- js_230/input-box/main.js
+++ js_230/input-box/main.js
@@ -1,12 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   let textField = document.querySelector(".text-field");
+  let content = document.querySelector(".content");
+  let cursorInterval;
 
   textField.addEventListener("click", event => {
     event.stopPropagation();
     textField.classList.add("focused");
+
+    if (!cursorInterval) {
+      cursorInterval = setInterval(() => textField.classList.toggle("cursor"), 500);
+    }
+  });
+
+  document.addEventListener("keydown", event => {
+    if (textField.classList.contains("focused")) {
+      if (event.key === "Backspace") {
+       content.textContent = content.textContent.slice(0, content.textContent.length - 1);
+      } else {
+        content.textContent += event.key;
+      }
+    }
   });
 
   document.addEventListener("click", () => {
-    textField.classList.remove("focused");
+    clearInterval(cursorInterval);
+    if (textField.classList.contains("focused")) {
+      textField.classList.remove("focused");
+      textField.classList.remove("cursor");
+    }
   });
 });
