25b1888abf8764194fd465da2ecd064927b270aa
Henry Yeary <henmofb@gmail.com>
2023-05-13 12:30:51 -0500

    Added custom event solution for bold element event exercise.

--- js_230/events-and-async-programming/bold-element-custom.js
+++ js_230/events-and-async-programming/bold-element-custom.js
@@ -1,13 +1,33 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   let paragraphElement = document.querySelector("p");
+
+//   function makeBold(element, callback) {
+//     element.style.fontWeight = "bold";
+
+//     if (callback && typeof callback === "function") {
+//       callback(element);
+//     }
+//   }
+
+//   makeBold(paragraphElement);
+// });
+
+// Using a custom event
+
 document.addEventListener("DOMContentLoaded", () => {
-  let paragraphElement = document.querySelector("p");
+  let sectionElement = document.querySelector("section");
 
-  function makeBold(element, callback) {
+  function makeBold(element) {
     element.style.fontWeight = "bold";
+    let bolded = new CustomEvent("bolded");
 
-    if (callback && typeof callback === "function") {
-      callback(element);
-    }
+    element.dispatchEvent(bolded);
   }
 
-  makeBold(paragraphElement);
-});
+  sectionElement.addEventListener("bolded", (event) => {
+    alert(event.target.tagName);
+    event.target.classList.add("highlight");
+  });
+
+  makeBold(sectionElement);
+})
