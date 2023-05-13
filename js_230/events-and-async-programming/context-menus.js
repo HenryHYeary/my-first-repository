514e03fa1fb87b100ffbd5c38fbaf38c35a4920f
Henry Yeary <henmo@LAPTOP-A37FGP66.localdomain>
2023-05-13 13:23:39 -0500

    added file for context menu exercise.

---
+++ js_230/events-and-async-programming/context-menus.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const main = document.querySelector("main");
+  const sub = document.querySelector("#sub");
+  
+  main.addEventListener("contextmenu", event => {
+    event.preventDefault();
+    alert(event.target.tagName.toLowerCase());
+  });
+  
+  sub.addEventListener("contextmenu", event => {
+    event.stopPropagation();
+    event.preventDefault();
+    alert(event.target.getAttribute("id"));
+  })
+});
