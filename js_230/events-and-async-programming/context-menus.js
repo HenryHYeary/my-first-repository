d7db74033211780c221300348c75857e0ace6851
Henry Yeary <henmofb@gmail.com>
2023-05-14 17:11:45 -0500

    Readding context-menus exercise.

--- js_230/events-and-async-programming/context-menus.js
+++ js_230/events-and-async-programming/context-menus.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const main = document.querySelector("main");
   const sub = document.querySelector("#sub");
-  
+
   main.addEventListener("contextmenu", event => {
     event.preventDefault();
     alert(event.target.tagName.toLowerCase());
   });
-  
+
   sub.addEventListener("contextmenu", event => {
     event.stopPropagation();
     event.preventDefault();
