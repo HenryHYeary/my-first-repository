ef4aed1904bed83b66882fb3a85bfa352fd6facf
Henry Yeary <henmo@LAPTOP-A37FGP66.localdomain>
2023-06-28 13:51:23 -0500

    Added files for dom-exercises.

--- js_230/dom-exercises/child-nodes.js
+++ js_230/dom-exercises/child-nodes.js
@@ -25,31 +25,67 @@
 
 // Above is a flawed solution
 
-function walk(node, callback) {
-  callback(node);
+// function walk(node, callback) {
+//   callback(node);
 
-  for (let index = 0; index < node.childNodes.length; index++) {
-      walk(node.childNodes[index], callback);
-  }
-}
+//   for (let index = 0; index < node.childNodes.length; index++) {
+//       walk(node.childNodes[index], callback);
+//   }
+// }
+
+// function childNodes(nodeId) {
+//   let node = document.getElementById(nodeId);
+//   let directChildren;
+//   if (node.childNodes) {
+//       directChildren = node.childNodes.length;
+//   } else {
+//       directChildren = 0;
+//   }
+
+//   let totalChildren = -1;
+//   walk(node, (node) => {
+//       totalChildren++;
+//   });
+
+//   let indirectChildren = totalChildren - directChildren;
+
+//   return [directChildren, indirectChildren];
+// }
+
+// console.log(childNodes("4"));
+
+/*
+input: node ID
+output: an array indicating the number of direct child
+nodes in the first element, and indirect child nodes
+in the element of the array
+
+Data Structure: Should record the number of direct childNodes
+as a count variable. 
+
+Algorithm: Can use a while loop to traverse down from
+a parent node to increment the allChildren count. Can do this
+by adding to the all children count the value of each successive array's
+length to the count.
+*/
 
 function childNodes(nodeId) {
-  let node = document.getElementById(nodeId);
-  let directChildren;
-  if (node.childNodes) {
-      directChildren = node.childNodes.length;
-  } else {
-      directChildren = 0;
+  let selectedNode = document.getElementById(nodeId);
+
+  function walk(node, callback) {
+    callback(node);
+
+    for (let index = 0; index < node.childNodes.length; index++) {
+      walk(node.childNodes[index], callback);
+    }
   }
 
-  let totalChildren = -1;
-  walk(node, (node) => {
-      totalChildren++;
-  });
+  let directChildren = selectedNode.childNodes.length;
+  let indirectChildrenCount = -1;
 
-  let indirectChildren = totalChildren - directChildren;
-
-  return [directChildren, indirectChildren];
+  walk(selectedNode, (node) => {
+    indirectChildrenCount++;
+  })
 }
 
-console.log(childNodes("4"));
+console.log(childNodes("2"));
